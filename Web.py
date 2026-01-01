0615aef0d60e56c9f6edb05524100718c0d5dd2a
Lukash <lukashn159@gmail.com>
2026-01-01 18:53:57 +0545

    Update Web.py

--- Web.py
+++ Web.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 with open("Da-ggle.html", mode="r") as d:
     html = d.read()
 
-@app.route("/")
+with open("robot_s.txt", mode="r") as r:
+    robots = r.read()
 
+@app.route("/")
 def web():
     return html
+
+@app.route("/robots.txt")
+def robo():
+    return robots
+
+if __name__ == "__main__":
+    app.run()
+
